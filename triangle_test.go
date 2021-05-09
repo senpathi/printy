bd99f91770fe8fce5d7cf74d618bc0221bb85d1b
nipunapathirana <nsspathirana@gmail.com>
2021-05-09 13:21:32 +0530

    triangle simple test added

---
+++ triangle_test.go
@@ -0,0 +1,8 @@
+package printy
+
+import "testing"
+
+func TestPrintInTriangle(t *testing.T) {
+	sampleStr := `This is a sample string, again this is a sample string `
+	PrintInTriangle(sampleStr)
+}
