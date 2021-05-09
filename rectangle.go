0b9ad039c7d834bff85ab09cddb047e4d779da7b
nipunapathirana <nsspathirana@gmail.com>
2021-05-09 13:15:52 +0530

    rectangle print loop updated

--- rectangle.go
+++ rectangle.go
@@ -7,41 +7,46 @@ import (
 
 func PrintInRectangle(text string, lengthFactor, widthFactor int) {
 	length, width := calculateLengthAndWidth(len(text), lengthFactor, widthFactor)
-	fmt.Printf(getRectangleString(text, length+1, width))
+	fmt.Printf(getRectangleString(text, length+2, width+2))
 }
 
 func getRectangleString(text string, length, width int) string {
 	str := ""
 	count := 0
-	for h := 1; h <= width; h++ {
-		for w := 1; w <= length; w++ {
-			if h == 1 || h == width {
-				str += "*  "
-				continue
-			} else if w == length-1 {
-				str += "   "
-				continue
-			} else if w == 1 || w == length {
-				str += "*  "
-				continue
-			} else {
-				if count == len(text)-1 {
-					str = str + text[count:count+1] + "  "
-					count = count + 1
+	//lBorder := 2
+	for w := 1; w <= width; w++ {
+		for l := 1; l <= length; l++ {
+			if w == 1 || w == width {
+				if l == 1 {
+					str = str + "* *"
 					continue
 				}
-				if count == len(text)-2 {
-					str = str + text[count:count+2] + " "
-					count = count + 2
+				if l == length {
+					str = str + "* *"
 					continue
 				}
-				if count > len(text)-3 {
-					str += "   "
+				if l%2 == 1 {
+					str += "*"
 					continue
 				}
-				str += text[count : count+3]
-				count = count + 3
+				str += " "
+				continue
 			}
+			if l == 1 {
+				str = str + fmt.Sprintf("%-3s", "*")
+				continue
+			}
+			if l == length {
+				str = str + fmt.Sprintf("%3s", "*")
+				continue
+			}
+
+			if count > len(text)-1 {
+				str += " "
+				continue
+			}
+			str = str + text[count:count+1]
+			count = count + 1
 		}
 		str += "\n"
 	}
@@ -50,9 +55,13 @@ func getRectangleString(text string, length, width int) string {
 }
 
 func calculateLengthAndWidth(area int, lFac int, wFac int) (int, int) {
+
 	width := math.Sqrt(float64(area*wFac) / float64(lFac))
-	width = math.Ceil(width)
 	length := width * float64(lFac) / float64(wFac)
 
-	return int(length), int(width)
+	if int(math.Ceil(length)) > 60 {
+		return 60, int(math.Ceil(float64(area) / 60))
+	}
+
+	return int(math.Ceil(length)), int(math.Ceil(width))
 }
