51ab62f533073e5e14ecc546639d241e53a355fa
nipunapathirana <nsspathirana@gmail.com>
2021-05-09 13:18:07 +0530

    triangle logic implemented

---
+++ triangle.go
@@ -0,0 +1,44 @@
+package printy
+
+import (
+	"fmt"
+	"math"
+)
+
+func PrintInTriangle(text string, baseFactor, heightFactor int) {
+	height := calculateTriangleHeight(len(text))
+	fmt.Printf(getTriangleString(text, height))
+}
+
+func getTriangleString(text string, height int) string {
+	str := ""
+	count := 0
+	for i := 0; i < height; i++ {
+		for j := 0; j < height-i; j++ {
+			str += " "
+		}
+		for k := 0; k <= 2*i; k++ {
+			if i == height-1 && k%2 != 0 {
+				str += " "
+				continue
+			}
+			if i == height-1 || k == 0 || k == 2*i {
+				str += "*"
+				continue
+			}
+			if i > 2 && k > 2 && k < 2*i-2 && count < len(text) {
+				str += text[count : count+1]
+				count += 1
+				continue
+			}
+			str += " "
+		}
+		str += "\n"
+	}
+
+	return str
+}
+
+func calculateTriangleHeight(area int) int {
+	return int(math.Ceil(math.Sqrt(float64(area))))
+}
