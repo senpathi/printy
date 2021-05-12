88bd611ed4a5a96084289271a5c70bf548dd877e
nipunapathirana <nsspathirana@gmail.com>
2021-05-12 23:11:45 +0530

    circle example added

---
+++ example/circle/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/senpathi/printy"
+)
+
+func main() {
+	str := `This is a sample string to print in senpathi/printy Circle aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbccccccccc`
+	printy.PrintInCircle(str)
+}
